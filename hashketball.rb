0ef8995611535611da75eeaa36a045f23cac7798
Jonathan Xiao <jonathanx111@gmail.com>
2020-09-07 20:52:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -17,7 +17,8 @@ def game_hash
         :slam_dunks => 1
         },
         
-        "Reggie Evans" => {
+        {
+        player_name: "Reggie Evans"
         :number => 30,
         :shoe => 14,
         :points => 12,
