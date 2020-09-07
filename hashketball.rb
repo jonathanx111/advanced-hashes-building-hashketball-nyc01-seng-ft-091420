3edf446b5de42408b88d27b9ce757b5b23c86706
Jonathan Xiao <jonathanx111@gmail.com>
2020-09-07 20:56:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -29,7 +29,8 @@ def game_hash
         :slam_dunks => 7
         },
         
-        "Brook Lopez" => {
+        {
+        player_name: "Brook Lopez",
         :number => 11,
         :shoe => 17,
         :points => 17,
@@ -40,7 +41,8 @@ def game_hash
         :slam_dunks => 15
         },
         
-        "Mason Plumlee" => {
+        {
+        player_name: "Mason Plumlee",
         :number => 1,
         :shoe => 19,
         :points => 26,
@@ -51,7 +53,8 @@ def game_hash
         :slam_dunks => 5
         },
         
-        "Jason Terry" => {
+        {
+        player_name: "Jason Terry",
         :number => 31,
         :shoe => 15,
         :points => 19,
@@ -69,7 +72,8 @@ def game_hash
       :colors => ["Turquoise", "Purple"],
       :players => [
         
-        "Jeff Adrien" => {
+        {
+        player_name: "Jeff Adrien",
         :number => 4,
         :shoe => 18,
         :points => 10,
@@ -80,7 +84,8 @@ def game_hash
         :slam_dunks => 2
         },
         
-        "Bismack Biyombo" => {
+        {
+        player_name: "Bismack Biyombo",  
         :number => 0,
         :shoe => 16,
         :points => 12,
@@ -91,7 +96,8 @@ def game_hash
         :slam_dunks => 10
         },
         
-        "DeSagna Diop" => {
+         {
+        player_name: "DeSagna Diop",
         :number => 2,
         :shoe => 14,
         :points => 24,
@@ -102,7 +108,8 @@ def game_hash
         :slam_dunks => 5
         }, 
         
-        "Ben Gordon" => {
+        {
+        player_name: "Ben Gordon",  
         :number => 8,
         :shoe => 15,
         :points => 33,
@@ -113,7 +120,8 @@ def game_hash
         :slam_dunks => 0
         },
         
-        "Kemba Walker" => {
+        {
+        player_name: "Kemba Walker",
         :number => 33,
         :shoe => 15,
         :points => 6,
