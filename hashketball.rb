78794f87704532f70b4f15dad78731a28cbcd7dd
Jonathan Xiao <jonathanx111@gmail.com>
2020-09-07 20:28:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,24 @@
 # Write your code here!
+def game_hash
+  {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [
+        "Alan Anderson" => 
+        {  
+        :number =>  ,
+        :shoe =>  ,
+        :points =>  ,
+        :rebounds =>  ,
+        :assists =>  ,
+        :steals =>  ,
+        :blocks =>  ,
+        :slam dunks =>
+        }
+        ]
+    },
+    :away => {
+      
+  }
+end
